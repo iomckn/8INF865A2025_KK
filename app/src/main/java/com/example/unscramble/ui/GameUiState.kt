d0ad173eaf34a00e74b8c0c24448c309970403b5
iomckn <keungueukilliane@gmail.com>
2025-10-19 23:49:47 -0400

    Etape_9

--- app/src/main/java/com/example/unscramble/ui/GameUiState.kt
+++ app/src/main/java/com/example/unscramble/ui/GameUiState.kt
@@ -4,6 +4,7 @@ data class GameUiState(
     val currentScrambledWord: String = "",
     val currentWordCount: Int = 1,
     val isGuessedWordWrong: Boolean = false,
+    val isGameOver: Boolean = false,
     val score: Int = 0,
 
 )
